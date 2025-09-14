cf4a81ff1b9d5ab966ad656460a3c0e007ba06c7
Enviado98 <152241479+Enviado98@users.noreply.github.com>
2025-09-14 15:43:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Esto lee el archivo .env
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+DEFAULT_VIDEO_QUALITY = os.getenv("DEFAULT_VIDEO_QUALITY", "720p")
