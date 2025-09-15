86a66016ca834534a3156c8777d9b8e87316fff2
Enviado98 <152241479+Enviado98@users.noreply.github.com>
2025-09-14 22:10:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
-SESSION_NAME = os.environ.get("SESSION_NAME", "userbot_session")
-DEFAULT_VIDEO_QUALITY = os.environ.get("DEFAULT_VIDEO_QUALITY", "360p")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+DEFAULT_VIDEO_QUALITY = os.environ.get("DEFAULT_VIDEO_QUALITY", "720p")
